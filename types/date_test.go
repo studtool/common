10f5096c0396b469c5e13e71eea97a192e7b097c
denis96z <zinoviev.denis@yandex.ru>
2019-04-07 17:49:57 +0300

    date type added

---
+++ types/date_test.go
@@ -0,0 +1,44 @@
+package types
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestDate_MarshalJSON(t *testing.T) {
+	now := time.Now()
+	nowDate := Date(now)
+
+	b, err := nowDate.MarshalJSON()
+	if err != nil {
+		t.Errorf("Marshalling error: %v", err)
+	}
+
+	expected := makeDateString(now)
+	actual := string(b)
+
+	if actual != expected {
+		t.Errorf("Expected: %s; Actual: %s", expected, actual)
+	}
+}
+
+func TestDate_UnmarshalJSON(t *testing.T) {
+	now := time.Now()
+	nowDate := Date(now)
+
+	expected := makeDateString(now)
+	if err := nowDate.UnmarshalJSON([]byte(expected)); err != nil {
+		t.Errorf("Unmarshalling error: %v", err)
+	}
+
+	actual := makeDateString(time.Time(nowDate))
+
+	if actual != expected {
+		t.Errorf("Expected: %s; Actual: %s", actual, expected)
+	}
+}
+
+func makeDateString(v time.Time) string {
+	return fmt.Sprintf("%04d-%02d-%02d", v.Year(), v.Month(), v.Day())
+}
