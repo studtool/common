0f17bae0e281accefd6f2f73b16a0143d6c095a3
denis96z <zinoviev.denis@yandex.ru>
2019-04-25 18:40:40 +0300

    refactoring

--- queues/types/registration.go
+++ queues/types/registration.go
@@ -2,31 +2,8 @@ package qtypes
 
 //go:generate easyjson
 
-import (
-	"fmt"
-
-	"github.com/studtool/common/errs"
-)
-
 //easyjson:json
-type RegistrationEmailData map[string]interface{}
-
-func (d *RegistrationEmailData) Validate() *errs.Error {
-	fields := []string{"email", "token"}
-
-	if len(*d) != len(fields) {
-		return d.makeErr()
-	}
-
-	for _, f := range fields {
-		if _, ok := (*d)[f]; !ok {
-			return d.makeErr()
-		}
-	}
-
-	return nil
-}
-
-func (d *RegistrationEmailData) makeErr() *errs.Error {
-	return errs.NewInternalError(fmt.Sprintf("wrong email data format: %v", *d))
+type RegistrationEmailData struct {
+	Email string `json:"email"`
+	Token string `json:"token"`
 }
