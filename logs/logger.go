5fc4023e02fb562befe8f2bfba9dbf0df0065d39
Denis Zinovyev <zinoviev.denis@yandex.ru>
2019-05-19 00:32:04 +0300

    stacktrace for logger

--- logs/logger.go
+++ logs/logger.go
@@ -1,6 +1,9 @@
 package logs
 
 import (
+	"fmt"
+	"runtime"
+
 	"github.com/sirupsen/logrus"
 )
 
@@ -19,21 +22,36 @@ func NewLogger() *Logger {
 }
 
 func (log *Logger) Debug(args ...interface{}) {
-	log.logger.Debug(args...)
+	log.logger.WithFields(log.callerInfo()).Debug(args...)
 }
 
 func (log *Logger) Info(args ...interface{}) {
-	log.logger.Info(args...)
+	log.logger.WithFields(log.callerInfo()).Info(args...)
 }
 
 func (log *Logger) Warning(args ...interface{}) {
-	log.logger.Warn(args...)
+	log.logger.WithFields(log.callerInfo()).Warn(args...)
 }
 
 func (log *Logger) Error(args ...interface{}) {
-	log.logger.Error(args...)
+	log.logger.WithFields(log.callerInfo()).Error(args...)
 }
 
 func (log *Logger) Fatal(args ...interface{}) {
-	log.logger.Fatal(args...)
+	log.logger.WithFields(log.callerInfo()).Fatal(args...)
+}
+
+func (log *Logger) callerInfo() logrus.Fields {
+	fpcs := make([]uintptr, 2)
+	caller := runtime.FuncForPC(fpcs[0] - 2)
+
+	name := caller.Name()
+	file, line := caller.FileLine(fpcs[0] - 2)
+
+	return logrus.Fields{
+		"func": name,
+		"file": fmt.Sprintf(
+			"%s:%d", file, line,
+		),
+	}
 }
