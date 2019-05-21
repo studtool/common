34bf96c1c86c40460e4f66403fc50be9f3bde740
Denis Zinovyev <zinoviev.denis@yandex.ru>
2019-05-21 21:37:08 +0300

    logger refactoring

--- go.mod
+++ go.mod
@@ -5,7 +5,8 @@ go 1.12
 require (
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/mailru/easyjson v0.0.0-20190403194419-1ea4449da983
-	github.com/sirupsen/logrus v1.4.1
+	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/testify v1.3.0 // indirect
-	golang.org/x/sys v0.0.0-20190509141414-a5b02f93d862 // indirect
+	go.uber.org/dig v1.7.1-0.20190429222441-802e703cc9e5
+	golang.org/x/sys v0.0.0-20190520201301-c432e742b0af // indirect
 )
--- logs/logger.go
+++ logs/logger.go
@@ -1,39 +1,18 @@
 package logs
 
-import (
-	"github.com/sirupsen/logrus"
-)
-
-type Logger struct {
-	logger *logrus.Logger
+type Logger interface {
+	Debug(args ...interface{})
+	Debugf(format string, args ...interface{})
+	Info(args ...interface{})
+	Infof(format string, args ...interface{})
+	Warning(args ...interface{})
+	Warningf(format string, args ...interface{})
+	Error(args ...interface{})
+	Errorf(format string, args ...interface{})
+	Fatal(args ...interface{})
+	Fatalf(format string, args ...interface{})
 }
 
-func NewLogger() *Logger {
-	return &Logger{
-		logger: func() *logrus.Logger {
-			log := logrus.StandardLogger()
-			log.SetFormatter(&logrus.JSONFormatter{})
-			return log
-		}(),
-	}
-}
-
-func (log *Logger) Debug(args ...interface{}) {
-	log.logger.Debug(args...)
-}
-
-func (log *Logger) Info(args ...interface{}) {
-	log.logger.Info(args...)
-}
-
-func (log *Logger) Warning(args ...interface{}) {
-	log.logger.Warn(args...)
-}
-
-func (log *Logger) Error(args ...interface{}) {
-	log.logger.Error(args...)
-}
-
-func (log *Logger) Fatal(args ...interface{}) {
-	log.logger.Fatal(args...)
+func NewLogger() Logger {
+	return NewRawLogger()
 }
